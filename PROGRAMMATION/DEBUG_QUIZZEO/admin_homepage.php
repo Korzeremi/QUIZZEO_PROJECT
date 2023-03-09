21ed421bc0c4245810566a0dbd6c57e9af971368
redes19 <120016236+redes19@users.noreply.github.com>
2023-03-09 20:56:32 +0100

    REMOVE OLD WEB SITE

--- PROGRAMMATION/DEBUG_QUIZZEO/admin_homepage.php
+++ PROGRAMMATION/DEBUG_QUIZZEO/admin_homepage.php
@@ -4,24 +4,53 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="admin_homepage.css">
     <title>QUIZZEO</title>
 </head>
 <body>
-    <navbar>
-        <a href="admin_homepage.php"><img alt="logo"></a>
-        <a href="disconnect.php"><input type="button" value="Se déconnecter"></a>
-        <a href="admin_panel.php"><input type="button" value="Panneau d'administration"></a>
-        <a href="admin_profile.php"><input type="button" value="Mon profil"></a>
-        <span class="mode_btn">
-                <input type="checkbox" name="theme-mode">
-                <span class="slider"></span>
-        </span>
-    </navbar>
+<header>
+        <div class="tete">
+            <div class="logo">
+                <a href="admin_homepage.php"><img src="logo.png" alt="logo"></a>
+            </div>
+            <div class="connect_btn">
+                <div class="connect">
+                    <a href="disconnect.php"><input type="button" value="Se déconnecter" class="button_head"></a>
+                </div>
+                <div class="mode_btn">
+                    <button for="themeSwitch" id="themeLogo" style="font-size: 90px;"><h3>DARK</h3></button>
+                    <!-- <input type="checkbox" name="theme-mode" class="checkbox"> -->
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <div class="button">
+    <a href="admin_panel.php"><input type="button" value="Panneau d'administration" class="button_ajout"></a>
+        <a href="admin_profile.php"><input type="button" value="Mon profil" class="button_ajout"></a>
+    </div>
+
     
-    <form action="admin_search.php" method="post">
-            <input type="text" id="searchbar" name="searchbar">
-            <input type="submit" value="Rechercher"></input>
-    </form>
+    <div class="navbar">
+        <div class="h2">
+            <a href="homepage.php"><h2>Nos quizs</h2></a>
+        </div>
+        <div class="searchform">
+            <form action="homepage_search.php" method="post">
+                <input class="searchbar" type="text" id="searchbar" name="searchbar" placeholder="Rechercher par nom">
+                <input class="searchbtn" type="submit" value="Rechercher"></input>
+            </form>
+        </div>
+        <div class="sortbyselect">
+            <select class="sortby" name="sortby" id="sortbyselectid">
+                <option selected="" value="">Trier par</option>
+                <option value="a">a</option>
+                <option value="b">b</option>
+                <option value="c">c</option>
+            </select>
+        </div>
+    </div>
+    
 
     <h2>Page d'accueil administrateur</h2>
     <?php   
@@ -64,5 +93,9 @@
             echo " ";
         }
     ?>
+
+    <div class="grid">
+            <div class="quiz"></div><div class="quiz"></div><div class="quiz"></div><div class="quiz"></div><div class="quiz"></div><div class="quiz"></div><div class="quiz"></div><div class="quiz"></div><div class="quiz"></div><div class="quiz"></div><div class="quiz"></div><div class="quiz"></div><div class="quiz"></div><div class="quiz"></div><div class="quiz"></div><div class="quiz"></div><div class="quiz"></div><div class="quiz"></div><div class="quiz"></div><div class="quiz"></div><div class="quiz"></div><div class="quiz"></div><div class="quiz"></div><div class="quiz"></div>
+        </div>
 </body>
 </html>
--- PROGRAMMATION/DEBUG_QUIZZEO/quizzer_homepage.php
+++ PROGRAMMATION/DEBUG_QUIZZEO/quizzer_homepage.php
@@ -4,21 +4,53 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="quizzer_homepage.css">
     <title>QUIZZEO</title>
 </head>
 <body>
-    <navbar>
-        <a href="quizzer_homepage.php"><img alt="logo"></a>
-        <a href="disconnect.php"><input type="button" value="Se déconnecter"></a>
-        <a href="disconnect.php"><input type="button" value="Gérer mes quizz"></a>  
-        <a href="disconnect.php"><input type="button" value="Créer un quiz"></a> 
-        <a href="quizzer_profile.php"><input type="button" value="Mon profil"></a> 
-    </navbar>
+    <header>
+        <div class="tete">
+            <div class="logo">
+                <a href="admin_homepage.php"><img src="logo.png" alt="logo"></a>
+            </div>
+            <div class="connect_btn">
+                <div class="connect">
+                    <a href="disconnect.php"><input type="button" value="Se déconnecter" class="button_head"></a>
+                </div>
+                <div class="mode_btn">
+                    <button for="themeSwitch" id="themeLogo" style="font-size: 90px;"><h3>DARK</h3></button>
+                    <!-- <input type="checkbox" name="theme-mode" class="checkbox"> -->
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <div class="button">
+        <a href="disconnect.php"><input type="button" value="Gérer mes quizz" class="button_ajout"></a>  
+        <a href="disconnect.php"><input type="button" value="Créer un quiz" class="button_ajout"></a> 
+        <a href="quizzer_profile.php"><input type="button" value="Mon profil" class="button_ajout"></a> 
+    </div>
+
     
-    <form action="quizzer_search.php" method="post">
-            <input type="text" id="searchbar" name="searchbar">
-            <input type="submit" value="Rechercher"></input>
-    </form>
+    <div class="navbar">
+        <div class="h2">
+            <a href="homepage.php"><h2>Nos quizs</h2></a>
+        </div>
+        <div class="searchform">
+            <form action="homepage_search.php" method="post">
+                <input class="searchbar" type="text" id="searchbar" name="searchbar" placeholder="Rechercher par nom">
+                <input class="searchbtn" type="submit" value="Rechercher"></input>
+            </form>
+        </div>
+        <div class="sortbyselect">
+            <select class="sortby" name="sortby" id="sortbyselectid">
+                <option selected="" value="">Trier par</option>
+                <option value="a">a</option>
+                <option value="b">b</option>
+                <option value="c">c</option>
+            </select>
+        </div>
+    </div>
 
     <h2>Page d'accueil quizzer</h2>
     <?php   
@@ -52,5 +84,19 @@
             
             $conn->close();
         ?>
+
+    <script>
+        const html = document.getElementsByTagName("html")[0];
+        const themeSwicth = document.getElementById("themeLogo");
+        themeSwicth.addEventListener("click", () => {
+        html.classList.toggle("nuit");
+        if (html.classList.contains("nuit")) {
+            themeSwicth.innerHTML = 'LIGHT'.fontsize(4);
+        } else {
+            themeSwicth.innerHTML = 'DARK'.fontsize(4);
+        }
+    });
+    </script> 
+
 </body>
 </html>
